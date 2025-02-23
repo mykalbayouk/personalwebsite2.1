f891b31ff43381b031a61d04812e8776a6cdcce0
Michael Bayouk <michaelbayouk@gmail.com>
2025-02-22 22:44:51 -0500

    navbar tweaks

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -29,7 +29,7 @@ const navItems = {
 }
 
 export function Navbar() {
-  const [hasScrolled, setHasScrolled] = useState(false);
+  const [hasScrolled, setHasScrolled] = useState(true);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -43,16 +43,16 @@ export function Navbar() {
   return (
     <aside className='fixed top-0 left-0 right-0 z-50 transition-all duration-200'>
       <div className="max-w-4xl mx-auto px-4">
-        <div className={`flex justify-between items-center h-16 ${hasScrolled ? 'bg-gray-800/70 dark:bg-gray-900/70 backdrop-blur-lg rounded-3xl my-2' : ''
+        <div className={`flex justify-between items-center h-12 ${hasScrolled ? 'bg-gray-800/70 backdrop-blur-lg rounded-3xl my-8 mx-5' : ''
           }`}>
-          <nav className="flex items-center overflow-x-auto scrollbar-hide">
+          <nav className="flex items-center overflow-x-auto scrollbar-show">
             <div className="flex space-x-4 min-w-max ml-4">
               {Object.entries(navItems).map(([path, { name, icon }]) => {
                 return (
                   <Link
                     key={path}
                     href={path}
-                    className="transition-all hover:text-purple-500 dark:hover:text-purple-500 flex align-middle relative py-1 px-2"
+                    className="transition-all hover:text-purple-500 flex align-middle relative py-1 px-2"
                   >
                     <div className="flex items-center">
                       {React.createElement(icon, {
